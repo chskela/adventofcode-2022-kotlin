c9e2c94070ed2b4c2d83ada08d39323b56f73a05
Chistyakov Alexey <chskela@gmail.com>
2022-12-04 23:38:55 +0300

    Solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val data = input.split("\n\n").map {str ->
+            str.lines().sumOf { it.toInt() }
+        }
+        return data.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        val data = input.split("\n\n").map {str ->
+            str.lines().sumOf { it.toInt() }
+        }
+        return data.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = File("src/Day01_test.txt").readText()
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = File("src/Day01.txt").readText()
+//    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
