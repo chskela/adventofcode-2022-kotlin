7a7d91ae2f2da90e4f40a3adb895e71f24a8b82f
Chistyakov Alexey <chskela@gmail.com>
2022-12-06 18:54:59 +0300

    Solver Day02

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,13 +1,23 @@
 package day01
 
 import java.io.File
+import java.util.PriorityQueue
 
 fun main() {
     fun parseInput(input: String) = input.split("\n\n").map { str ->
         str.lines().map { it.toInt() }
     }
 
-    fun List<List<Int>>.topNElves(n: Int): Int = map { it.sum() }.sortedDescending().take(n).sum()
+    fun List<List<Int>>.topNElves(n: Int): Int {
+        val best = PriorityQueue<Int>()
+        for (calories in map { it.sum() }) {
+            best.add(calories)
+            if (best.size > n) {
+                best.poll()
+            }
+        }
+        return best.sum()
+    }
 
     fun part1(input: String): Int {
         val data = parseInput(input)
@@ -22,9 +32,10 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = File("src/day01//Day01_test.txt").readText()
     check(part1(testInput) == 24000)
+    println(part1(testInput))
+    println(part2(testInput))
 
     val input = File("src/day01/Day01.txt").readText()
-//    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
---
+++ src/day02/Day02.kt
@@ -0,0 +1,52 @@
+package day02
+
+import java.io.File
+
+fun main() {
+
+    fun parseInput(input: String) = input.lines().map { it.split(" ") }.map { it.first() to it.last() }
+
+    val points = mapOf("X" to 1, "Y" to 2, "Z" to 3)
+    val winsPlayer = listOf("A" to "Y", "B" to "Z", "C" to "X")
+    val drawPlayer = listOf("A" to "X", "B" to "Y", "C" to "Z")
+    val lossPlayer = listOf("A" to "Z", "B" to "X", "C" to "Y")
+
+    fun getPointPerRound(pair: Pair<String, String>): Int = when (pair) {
+        in winsPlayer -> 6
+        in drawPlayer -> 3
+        else -> 0
+    }
+
+    fun part1(input: String): Int {
+        val data = parseInput(input)
+            .fold(0) { acc, pair ->
+                acc + getPointPerRound(pair) + points.getOrDefault(pair.second, 0)
+            }
+        return data
+    }
+
+    fun part2(input: String): Int {
+        val data = parseInput(input)
+            .map {(first, second) ->
+                when(second){
+                    "Z" -> first to (winsPlayer.toMap()[first] ?: first)
+                    "Y" -> first to (drawPlayer.toMap()[first] ?: first)
+                    else -> first to (lossPlayer.toMap()[first] ?: first)
+                }
+            }
+            .fold(0) { acc, pair ->
+                acc + getPointPerRound(pair) + points.getOrDefault(pair.second, 0)
+            }
+        return data
+    }
+
+    val testInput = File("src/day02/Day02_test.txt").readText()
+
+    println(part1(testInput))
+    println(part2(testInput))
+
+    val input = File("src/day02/Day02.txt").readText()
+
+    println(part1(input))
+    println(part2(input))
+}
